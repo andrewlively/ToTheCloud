1c8d3adb40e6c771a386d517b110816e8fd4677f
Andrew Lively <andrew.lively2@gmail.com>
2014-10-28 01:27:41 -0400

    Started file upload

--- app/routes.js
+++ app/routes.js
@@ -1,21 +1,81 @@
  // app/routes.js
 
 
- module.exports = function (app) {
+ module.exports = function (app, passport) {
 
      // server routes ===========================================================
      // handle things like api calls
      // authentication routes
 
-     // frontend routes =========================================================
-     app.post('/login', function (req, res) {
-         console.log(req.body);
-         res.redirect('/');
+     app.get('/api/files', isLoggedIn, function (req, res) {
+         res.status(200).json([{
+             _id: 123,
+             type: 'file',
+             name: 'testfile.html',
+             dateCreated: new Date(),
+             lastModified: new Date()
+         }, {
+             _id: 456,
+             type: 'folder',
+             name: 'Images',
+             dateCreated: new Date(),
+             lastModified: new Date()
+         }, {
+             _id: 789,
+             type: 'file',
+             name: 'app.js',
+             dateCreated: new Date(),
+             lastModified: new Date()
+         }]).end();
      });
      
-     // route to handle all angular requests
-     app.get('/', function (req, res) {
-         res.render('partials/app', {});
+     app.post('/api/upload', isLoggedIn, function (req, res) {
+         console.log(req);
      });
 
+     // frontend routes =========================================================
+
+     app.get('/download/:fileId', isLoggedIn, function (req, res) {
+         console.log(req.params.fileId);
+         // TODO: Find file
+         res.status(200).sendFile(require('path').join(__dirname + '/../views/head.ejs'));
+     });
+
+     app.post('/login', passport.authenticate('local-login', {
+         successRedirect: '/', // redirect to the secure profile section
+         failureRedirect: '/', // redirect back to the signup page if there is an error
+         failureFlash: true // allow flash messages
+     }));
+
+     app.get('/logout', function (req, res) {
+         req.logout();
+         res.redirect('/');
+     });
+
+     // route to handle all angular requests
+     app.get('/', function (req, res) {
+         if (req.isAuthenticated()) {
+             res.render('partials/app', {
+                 user: req.user
+             });
+         } else {
+             res.render('partials/login', {
+                 loginMessage: req.flash('loginMessage')
+             });
+         }
+     });
+
+     app.get('*', function (req, res) {
+         res.redirect('/');
+     });
  };
+
+ function isLoggedIn(req, res, next) {
+
+     // if user is authenticated in the session, carry on 
+     if (req.isAuthenticated())
+         return next();
+
+     // if they aren't redirect them to the home page
+     res.redirect('/');
+ }
--- server.js
+++ server.js
@@ -1,48 +1,69 @@
 // server.js
 
 // modules =================================================
-var express        = require('express');
-var app            = express();
-var bodyParser     = require('body-parser');
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
 var methodOverride = require('method-override');
+var cookieParser = require('cookie-parser');
+var session      = require('express-session');
+var passport = require('passport');
+var mongoose = require('mongoose');
+var flash = require('connect-flash');
 
 // configuration ===========================================
-    
+
 // config files
-//var db = require('./config/db');
+var db = require('./config/db');
 
 // set our port
-var port = process.env.PORT || 1337; 
+var port = process.env.PORT || 1337;
 
 // connect to our mongoDB database 
 // (uncomment after you enter in your own credentials in config/db.js)
-// mongoose.connect(db.url); 
+ mongoose.connect(db.mongodb.url); 
 
 // set the view engine to ejs
 app.set('view engine', 'ejs');
 
+app.use(cookieParser()); // read cookies (needed for auth)
+// required for passport
+app.use(session({
+    secret: 'ilovescotchscotchyscotchscotch',
+    resave: true,
+    saveUninitialized: true
+})); // session secret
+
+require('./config/passport')(passport);
+app.use(passport.initialize());
+app.use(passport.session()); // persistent login sessions
+app.use(flash());
+
 // get all data/stuff of the body (POST) parameters
 // parse application/json 
-app.use(bodyParser.json()); 
-
+app.use(bodyParser.json());
 // parse application/vnd.api+json as json
-app.use(bodyParser.json({ type: 'application/vnd.api+json' })); 
+app.use(bodyParser.json({
+    type: 'application/vnd.api+json'
+}));
 
 // parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: true })); 
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 
 // override with the X-HTTP-Method-Override header in the request. simulate DELETE/PUT
-app.use(methodOverride('X-HTTP-Method-Override')); 
+app.use(methodOverride('X-HTTP-Method-Override'));
 
 // set the static files location /public/img will be /img for users
-app.use(express.static(__dirname + '/public')); 
+app.use(express.static(__dirname + '/public'));
 
 // routes ==================================================
-require('./app/routes')(app); // configure our routes
+require('./app/routes')(app, passport); // configure our routes
 
 // start app ===============================================
 // startup our app at http://localhost:8080
-app.listen(port);               
+app.listen(port);
 
 // shoutout to the user                     
 console.log('Magic happens on port ' + port);
