1b16378f6553240057430df94816574cb5dd7323
Andrew Lively <andrew.lively2@gmail.com>
2014-11-20 15:54:14 -0500

    Added byte filter with support up to gb

--- public/js/filters/byteFilter.js
+++ public/js/filters/byteFilter.js
@@ -1,5 +1,16 @@
 app.filter('byteFilter', function () {
-    return function (input) {
-        return input;
+    return function (bytes) {
+        var units = ' B';
+        var divisor = 1;
+        
+        if (bytes < 1000000) {
+            units = ' KB';
+            divisor = 1000;
+        } else if (bytes < 1000000000) {
+            units = ' MB';
+            divisor = 1000000;
+        }
+        
+        return (bytes / divisor).toFixed(1) + units;
     };
 });
