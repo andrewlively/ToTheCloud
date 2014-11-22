2a61de1b21209fa9bb2860a0aa5c7e050dc9e839
Andrew Lively <andrew.lively2@gmail.com>
2014-11-22 11:51:48 -0500

    Readding defaults

---
+++ config/sendgrid.js
@@ -0,0 +1,4 @@
+module.exports = {
+    api_user: 'ENTER_API_USER_HERE',
+    api_key: 'ENTER_API_KEY_HERE'
+};
---
+++ config/systemSettings.js
@@ -0,0 +1,9 @@
+module.exports = {
+    host: {
+        domain: 'ENTER_DOMAIN_HERE',
+        port: 1337
+    },
+    support: {
+        email: 'ENTER_SUPPORT_EMAIL_HERE'
+    }
+};
