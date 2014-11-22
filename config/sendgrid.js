6ca568b72cf666a1e2ef840531989237dc258084
Andrew Lively <andrew.lively2@gmail.com>
2014-11-22 11:49:35 -0500

    Readded defaults

--- config/sendgrid.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-    api_user: 'ENTER_API_USER_HERE',
-    api_key: 'ENTER_API_KEY_HERE'
-};
--- config/systemSettings.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    host: {
-        domain: 'http://cloud.andrewlively.com/',
-        port: 1337
-    },
-    support: {
-        email: 'support@andrewlively.com'
-    }
-};
